219b05a46c65702f6827efb0fa10454d3c622165
友浦萌菜美 <oxmnmfmnmxo2024@gmail.com>
2025-09-14 17:53:37 +0900

    FizzBuzz

--- index06.php
+++ index06.php
@@ -1,4 +1,23 @@
 <?php
-for ($i=0; $i<=5; ++$i){
-    echo $i;
+$Fizz = "Fizz";
+$Buzz = "Buzz";
+$FizzBuzz = "FizzBuzz";
+
+for ($i=1; $i<=50; $i++){
+    if($i%3==0 && $i%5==0){
+        echo $FizzBuzz . " ";
+    }
+    elseif($i%5==0){
+        echo $Buzz . " ";
+    }
+    elseif($i%3==0){
+        echo $Fizz . " ";
+    }
+    else{
+        echo $i . " ";
+    }
 }
+
+// cd /Users/mnm_tmur/coachtech/self-study/php
+// php -S localhost:8000
+// http://localhost:8000/index06.php
